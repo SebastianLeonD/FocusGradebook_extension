f67f3bb25bcc593e520c3629e4ece389c1e2bc22
Sebastian Leon <sebastian.08ld@gmail.com>
2025-06-06 15:58:16 -0400

    Delete FocusGradebook_extension directory

--- FocusGradebook_extension/background.js
+++
@@ -1,7 +0,0 @@
-// Background script - handles extension icon clicks
-chrome.action.onClicked.addListener((tab) => {
-  // Only work on Focus websites
-  if (tab.url && tab.url.includes('focusschoolsoftware.com/focus/Modules.php?modname=Grades')) {
-    chrome.tabs.sendMessage(tab.id, {type: "openFloatingCalculator"});
-  }
-});
